f6e14f751d2e11317c8eab4f099740d134409fe0
mikmself <mikmself@gmail.com>
2024-05-24 15:39:26 +0700

    feat : add sidebarx & customize

---
+++ lib/component/sidebar.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:sidebarx/sidebarx.dart';
+
+void main() {
+  runApp(SidebarXExampleApp());
+}
+
+class SidebarXExampleApp extends StatelessWidget {
+  SidebarXExampleApp({Key? key}) : super(key: key);
+
+  final _controller = SidebarXController(selectedIndex: 0, extended: true);
+  final _key = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SidebarX Example',
+      debugShowCheckedModeBanner: false,
+      home: Builder(
+        builder: (context) {
+          final isSmallScreen = MediaQuery.of(context).size.width < 600;
+          return Scaffold(
+            key: _key,
+            appBar: isSmallScreen
+                ? AppBar(
+              backgroundColor: canvasColor,
+              title: Text("Map Voters"),
+              leading: IconButton(
+                onPressed: () {
+                  _key.currentState?.openDrawer();
+                },
+                icon: const Icon(Icons.menu),
+              ),
+            )
+                : null,
+            drawer: ExampleSidebarX(controller: _controller),
+            body: Row(
+              children: [
+                if (!isSmallScreen) ExampleSidebarX(controller: _controller),
+                Expanded(
+                  child: Center(
+                    child: _ScreensExample(
+                      controller: _controller,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ExampleSidebarX extends StatelessWidget {
+  const ExampleSidebarX({
+    Key? key,
+    required SidebarXController controller,
+  })  : _controller = controller,
+        super(key: key);
+
+  final SidebarXController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return SidebarX(
+      controller: _controller,
+      theme: SidebarXTheme(
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: canvasColor,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        hoverColor: scaffoldBackgroundColor,
+        textStyle: TextStyle(color: Colors.white.withOpacity(0.7)),
+        selectedTextStyle: const TextStyle(color: Colors.white),
+        hoverTextStyle: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w500,
+        ),
+        itemTextPadding: const EdgeInsets.only(left: 30),
+        selectedItemTextPadding: const EdgeInsets.only(left: 30),
+        itemDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: canvasColor),
+        ),
+        selectedItemDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: actionColor.withOpacity(0.37),
+          ),
+          gradient: const LinearGradient(
+            colors: [accentCanvasColor, canvasColor],
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.28),
+              blurRadius: 30,
+            )
+          ],
+        ),
+        iconTheme: IconThemeData(
+          color: Colors.white.withOpacity(0.7),
+          size: 20,
+        ),
+        selectedIconTheme: const IconThemeData(
+          color: Colors.white,
+          size: 20,
+        ),
+      ),
+      extendedTheme: const SidebarXTheme(
+        width: 300,
+        decoration: BoxDecoration(
+          color: canvasColor,
+        ),
+      ),
+      footerDivider: divider,
+      headerBuilder: (context, extended) {
+        return Padding(padding: EdgeInsets.only(top: 40));
+      },
+      items: [
+        SidebarXItem(
+          icon: Icons.home,
+          label: 'DASHBOARD',
+
+        ),
+        const SidebarXItem(
+          icon: Icons.account_tree,
+          label: 'KOORDINATOR',
+        ),
+        const SidebarXItem(
+          icon: Icons.account_box,
+          label: 'SAKSI',
+        ),
+        const SidebarXItem(
+          icon: Icons.people,
+          label: 'CALON PEMILIH',
+        ),
+        const SidebarXItem(
+          icon: Icons.map,
+          label: 'PETA SUARA',
+        ),
+        const SidebarXItem(
+          icon: Icons.photo,
+          label: 'C1 SUARA',
+        ),
+        const SidebarXItem(
+          icon: Icons.settings,
+          label: 'PENGATURAN',
+        ),
+      ],
+    );
+  }
+
+  void _showDisabledAlert(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text(
+          'Item disabled for selecting',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
+
+class _ScreensExample extends StatelessWidget {
+  const _ScreensExample({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  final SidebarXController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Padding(padding: EdgeInsets.all(10));
+  }
+}
+const canvasColor = Color(0xFF27A18B);
+const scaffoldBackgroundColor = Color(0xFF70CED4);
+const accentCanvasColor = Color(0xFF27A18B);
+const white = Colors.white;
+final actionColor = const Color(0xFF5F5FA7).withOpacity(0.6);
+final divider = Divider(color: white.withOpacity(0.3), height: 1);
