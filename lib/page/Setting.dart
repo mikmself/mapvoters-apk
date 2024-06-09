79ac57bf3b3a5445a9f426a9cd4023f1db695413
mikmself <mikmself@gmail.com>
2024-06-09 23:34:52 +0700

    fix : fix upload photo

--- lib/page/Setting.dart
+++ lib/page/Setting.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mapvotersapk/page/Register/nextRegister.dart';
@@ -8,7 +11,7 @@ String? typeselect;
 String? partaiselect;
 
 class SettingPage extends StatefulWidget {
-  const SettingPage({super.key, required this.title});
+  const SettingPage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
@@ -21,10 +24,11 @@ class _SettingPageState extends State<SettingPage> {
   final TextEditingController _telephoneController = TextEditingController();
   final TextEditingController _newPasswordController = TextEditingController();
   final TextEditingController _partaiController = TextEditingController();
-  final TextEditingController _fotoController = TextEditingController();
   final TextEditingController _typeController = TextEditingController();
   final TextEditingController _nomorUrutController = TextEditingController();
   final TextEditingController _dapilController = TextEditingController();
+
+  File? _imageFile;
 
   @override
   Widget build(BuildContext context) {
@@ -66,15 +70,84 @@ class _SettingPageState extends State<SettingPage> {
                 list: partai,
                 onSelected: (value) {
                   setState(() {
-                    typeselect = value;
+                    partaiselect = value;
                   });
                 },
               ),
               const SizedBox(height: 20),
-              textfield(
-                controller: _fotoController,
-                obscure: false,
-                label: 'Foto',
+              SizedBox(
+                width: 350,
+                child: SizedBox(
+                  height: 170,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const SizedBox(width: 10),
+                      Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 80),
+                            Text(
+                              "FOTO :",
+                              style: GoogleFonts.getFont(
+                                'Nunito',
+                                fontWeight: FontWeight.w900,
+                                fontSize: 20,
+                                letterSpacing: 1,
+                                color: Colors.black54,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.black12,
+                                  fixedSize: const Size(140, 50),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                              onPressed: () {
+                                setState(() {
+                                  _pickAnyFile();
+                                });
+                              },
+                              child: Text(
+                                "PILIH FILE",
+                                style: GoogleFonts.getFont(
+                                  'Nunito',
+                                  fontWeight: FontWeight.w900,
+                                  fontSize: 17,
+                                  letterSpacing: 0.5,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 50),
+                      _imageFile != null
+                          ? wadahfoto(
+                        foto: _imageFile!,
+                      )
+                          : SizedBox(
+                        height: MediaQuery.of(context).size.height,
+                        width: 140,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(image: AssetImage('')),
+                            border: Border.all(),
+                            color: Colors.black12,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      )
+                    ],
+                  ),
+                ),
               ),
               const SizedBox(height: 20),
               dropdown(
@@ -106,10 +179,12 @@ class _SettingPageState extends State<SettingPage> {
               ElevatedButton(
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black12,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    fixedSize: Size(200, 50)),
+                  backgroundColor: Colors.black12,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  fixedSize: const Size(200, 50),
+                ),
                 child: Text(
                   "Update Profile",
                   style: GoogleFonts.getFont(
@@ -122,6 +197,68 @@ class _SettingPageState extends State<SettingPage> {
                 ),
               ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+  Future<void> _pickAnyFile() async {
+    FilePickerResult? result =
+    await FilePicker.platform.pickFiles(type: FileType.image);
+
+    if (result != null) {
+      File file = File(result.files.first.path!);
+      setState(() {
+        _imageFile = file;
+      });
+    } else {
+      setState(() {
+        _imageFile = null;
+      });
+    }
+  }
+}
+void _showImageDialog(BuildContext context, File imageFile) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return Dialog(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Container(
+            child: Image.file(
+              imageFile,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      );
+    },
+  );
+}
+class wadahfoto extends StatelessWidget {
+  final File foto;
+  const wadahfoto({
+    super.key,
+    required this.foto,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _showImageDialog(context, foto);
+      },
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: 140,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            image: DecorationImage(image: FileImage(foto), fit: BoxFit.cover),
+            border: Border.all(),
+            borderRadius: BorderRadius.circular(5),
           ),
         ),
       ),
