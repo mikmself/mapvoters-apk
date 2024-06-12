c61a957a0a5fbeefac862e1f3aee88873778d10d
charezt23 <kharis.subani@gmail.com>
2024-06-12 12:15:23 +0700

    add kelurahan

--- lib/component/data/GlobalVariable.dart
+++ lib/component/data/GlobalVariable.dart
@@ -1,2 +1,16 @@
-const String BASE_URL = "http://47.236.54.237/api";
+const String BASE_URL = "https://bef9-112-78-156-233.ngrok-free.app/api";
 String? token = "";
+
+Map<String, String> loginData = {
+  "paslonID": "1",
+  "userID": "2",
+  "partaiID": "1",
+  "nama": "Subani Irga Angelina Alam Sinurat",
+  "email": "siaas@gmail.com",
+  "telephone": "081233033303",
+  "role": "paslon",
+  "foto": "siaas.jpg",
+  "type": "dprri",
+  "nomor_urut": "1",
+  "dapil": "Jawa Tengah VIII",
+};
