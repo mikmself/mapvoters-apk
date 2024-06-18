317fa52b2de6bd6b1203e2d81b4332bf77cb2c94
charezt23 <kharis.subani@gmail.com>
2024-06-18 16:34:42 +0700

    'fix: seacrh by list'

--- lib/component/data/GlobalVariable.dart
+++ lib/component/data/GlobalVariable.dart
@@ -1,16 +1,24 @@
-const String BASE_URL = "https://539c-112-78-156-225.ngrok-free.app/api";
+const String BASE_URL = "https://2bcc-112-78-156-225.ngrok-free.app/api";
 String? token = "";
 
 Map<String, String> loginData = {
-  "paslonID" : "1",
-  "userID" : "2",
-  "partaiID" : "1",
-  "nama" : "Subani Irga Angelina Alam Sinurat",
-  "email" : "siaas@gmail.com",
-  "telephone" : "081233033303",
-  "role" : "paslon",
-  "foto" : "siaas.jpg",
-  "type" : "dprri",
-  "nomor_urut" : "1",
-  "dapil" : "Jawa Tengah VIII",
+  "paslonID": "1",
+  "userID": "2",
+  "partaiID": "1",
+  "nama": "Subani Irga Angelina Alam Sinurat",
+  "email": "siaas@gmail.com",
+  "telephone": "081233033303",
+  "role": "paslon",
+  "foto": "siaas.jpg",
+  "type": "dprri",
+  "nomor_urut": "1",
+  "dapil": "Jawa Tengah VIII",
 };
+
+List<LoginModel> login = [];
+
+class LoginModel {
+  int? id;
+  String? role;
+  LoginModel({this.id, this.role});
+}
