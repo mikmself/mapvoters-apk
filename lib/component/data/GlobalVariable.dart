4c708d5c5e40a0b40a37f47df413dfa688b71afb
mikmself <mikmself@gmail.com>
2024-06-18 13:19:43 +0700

    feat : impelement api to dashboard

--- lib/component/data/GlobalVariable.dart
+++ lib/component/data/GlobalVariable.dart
@@ -1,8 +1,8 @@
-const String BASE_URL = "https://539c-112-78-156-225.ngrok-free.app/api";
+const String BASE_URL = "https://b57e-112-78-156-227.ngrok-free.app/api";
 String? token = "";
 
-Map<String, String> loginData = {
-  "paslonID" : "1",
+Map<String, dynamic> loginData = {
+  "paslonID" : 1,
   "userID" : "2",
   "partaiID" : "1",
   "nama" : "Subani Irga Angelina Alam Sinurat",
