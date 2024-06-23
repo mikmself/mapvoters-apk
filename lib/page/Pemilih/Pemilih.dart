9d51af363b2e490a09b2cdfcb7e886ddcb59801d
DinaSinurat05 <dimasinurat2021@gmail.com>
2024-06-23 12:35:55 +0700

    feat: perubahan controller pemilih

--- lib/page/Pemilih/Pemilih.dart
+++ lib/page/Pemilih/Pemilih.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mapvotersapk/component/controller/PemilihController.dart';
 import 'package:mapvotersapk/component/data/ListData.dart';
 import 'package:mapvotersapk/component/sidebar.dart';
 import 'package:mapvotersapk/page/Pemilih/Create_Pemilih.dart';
@@ -6,108 +7,47 @@ import 'package:mapvotersapk/page/Register/metod.dart';
 
 methodRegister getdata = methodRegister();
 
-class Pemilih extends StatelessWidget {
-  final String judul;
-  final List list;
-
+class Pemilih extends StatefulWidget {
   const Pemilih({
-    super.key,
-    required this.labeltext,
-    required this.judul,
-    required this.list, required this.title,
-  });
+    Key? key,
+    required this.title,
+  }) : super(key: key);
 
-  final String labeltext;
   final String title;
+
   @override
+  State<Pemilih> createState() => _PemilihState();
+}
+
+class _PemilihState extends State<Pemilih> {
+  int _currentIndex = 0;
+
+  void _changeIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+  
+  void _goBack() {
+  setState(() {
+    _currentIndex = 0;
+  });
+  }
+
+  
+ @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Stack(
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
         children: [
-          Column(
-            children: [
-              Container(
-                padding: EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(judul,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w800,
-                        )),
-                    IconButton(
-                      icon: Icon(Icons.arrow_back),
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => SidebarApp(),
-                          ),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: labeltext,
-                  prefixIcon: const Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Expanded(
-                  child: FutureBuilder(
-                future: getdata.showpemilih(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else {
-                    if (pemilihlist.length == 0) {
-                      return const Center(
-                        child: Text("data tidak ditemukaxn"),
-                      );
-                    }
-                  }
-                  return ListView.builder(
-                    itemCount: list.length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        margin: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: ListTile(
-                          title: Text(list[index].nama!), //perbedaan
-                          trailing: IconButton(
-                              icon: const Icon(Icons.info),
-                              onPressed: () {
-                                // Seharusnya show pemilih
-                              }),
-                        ),
-                      );
-                    },
-                  );
-                },
-              )),
-            ],
+          GetAllDataPemilih(
+            onItemSelected: _changeIndex,
+            judul: 'pemilih',
+            list: pemilihlist,
           ),
-          Positioned(
-            bottom: 16,
-            right: 16,
-            child: FloatingActionButton(
-              onPressed: () => {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CreatePemilih(),
-                    ))
-              },
-              child: Icon(Icons.add),
-            ),
+          CreatePemilih(
+            onBack: () => _changeIndex(0),
           ),
         ],
       ),
