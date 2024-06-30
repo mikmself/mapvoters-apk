a6e3d469c619e1cfe48bc2f82cea4fa90111c6fb
Idasut17 <yusufnuralam0@gmail.com>
2024-07-01 05:50:06 +0700

    Feat : List untuk menampung dari data semua model

---
+++ lib/page/PemetaanSuara/model/ListModel.dart
@@ -0,0 +1,9 @@
+import 'package:apiapiapi/PemetaanSuara/model/modelKabupaten.dart';
+import 'package:apiapiapi/PemetaanSuara/model/modelKecamatan.dart';
+import 'package:apiapiapi/PemetaanSuara/model/modelKelurahan.dart';
+import 'package:apiapiapi/PemetaanSuara/model/modelProvinsi.dart';
+
+List<modelKelurahan> kelurahanList = [];
+List<ModelProvinsi> provinsiList = [];
+List<modelKecamatan> kecamatanList = [];
+List<modelKabupaten> kabupatenList = [];
