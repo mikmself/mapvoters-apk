f7442bdc03af341f96be436b6961354dd512d152
Ellazian <mariaangelinacc1@gmail.com>
2024-07-03 00:25:05 +0700

    fix saksi

--- lib/page/Saksi/saksi.dart
+++ lib/page/Saksi/saksi.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mapvotersapk/component/controller/SaksiController.dart';
 import 'package:mapvotersapk/component/data/ListData.dart';
 import 'package:mapvotersapk/page/Saksi/RegisterSaksi.dart';
+import 'package:mapvotersapk/component/service/SaksiService.dart';
 
 class Saksi extends StatefulWidget {
   const Saksi({
@@ -15,11 +16,24 @@ class Saksi extends StatefulWidget {
 }
 
 class _SaksiState extends State<Saksi> {
+  final SaksiService _saksiService = SaksiService();
   int _currentIndex = 0;
 
   void _changeIndex(int index) {
     setState(() {
       _currentIndex = index;
+    });
+  }
+
+    @override
+  void initState() {
+    super.initState();
+    _refreshSaksiList();
+  }
+
+  void _refreshSaksiList() {
+    setState(() {
+      _saksiService.GetAllDataSaksi();
     });
   }
 
@@ -30,6 +44,7 @@ class _SaksiState extends State<Saksi> {
         index: _currentIndex,
         children: [
           GetAllDataSaksi(
+            refresh: _refreshSaksiList,
             onItemSelected: _changeIndex,
             judul: 'Saksi',
             list: saksiListan,
